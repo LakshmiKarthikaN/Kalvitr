1fa5e5dc69315c8a617ead6d1f06512193d17531
Lakshmi Karthika <lakshmikarthika07@gmail.com>
2025-10-14 17:20:24 +0530

    change cors origin

--- src/main/java/com/kalvitrack_backend/controller/registrationfeature/StudentController.java
+++ src/main/java/com/kalvitrack_backend/controller/registrationfeature/StudentController.java
@@ -31,7 +31,7 @@ import java.util.UUID;
 @RequestMapping("/api/students")
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin(origins = {"https://kalvitrack.vercel.app"})
+@CrossOrigin(origins = {"*"})
 public class StudentController {
 
     private final StudentService studentService;
